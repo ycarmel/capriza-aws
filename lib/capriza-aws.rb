fd1486552e218be9f24fe05bb449b72df91d7d56
Alon Becker <alon@alonbecker.com>
2013-04-23 20:48:30 +0300

    Added make public support

--- lib/capriza-aws.rb
+++ lib/capriza-aws.rb
@@ -9,38 +9,59 @@ module Capriza
 
     class S3connect
 
-      def initialize (config_file)
-        unless File.exist?(config_file)
-          raise "Config file #{config_file} not found"
-        end
-        if File.extname(config_file) == '.yaml'
-          config = YAML.load(File.read(config_file))
-        elsif File.extname(config_file) == '.json'
-          config = JSON.load(File.read(config_file))
-        else
-          raise "Unrecognized file extension. Currently only support json and yaml"
-        end
-        unless config.kind_of?(Hash)
-          raise "Config file #{config_file} is formatted incorrectly. Please use the following format:" +
-                    "access_key_id: YOUR_ACCESS_KEY_ID" +
-                    "secret_access_key: YOUR_SECRET_ACCESS_KEY"  +
-                    "s3_endpoint: YOUR_S3_ENDPOINT"
-        end
-        AWS.config(config)
+      def initialize (config)
+        @config = config
+        self.validate
+        AWS.config(@config)
       end
 
+      def validate
+        if @config.kind_of?(String) and File.exist?(config_file)
+          if File.exist?(config_file)
+            self.read_config_file(@config)
+          else
+            raise "Config file #{config_file} not found"
+          end
+        elsif @config.kind_of?(Hash)
+           puts @config
+           unless @config.has_key?('access_key_id') and @config.has_key?('secret_access_key') and @config.has_key?('s3_endpoint')
+            raise "Configuration missing. need the following " +
+                      "access_key_id: YOUR_ACCESS_KEY_ID " +
+                      "secret_access_key: YOUR_SECRET_ACCESS_KEY "  +
+                      "s3_endpoint: YOUR_S3_ENDPOINT "
+           end
+        else
+          raise("Unrecognized Format. Config can be an filename or Hash.")
+        end
+      end
+
+      def read_config_file(config_file)
+        case File.extname(config_file)
+          when '.yaml'
+            @config = YAML.load(File.read(config_file))
+          when '.json'
+            @config = JSON.load(File.read(config_file))
+          else
+            raise "Unrecognized file extension. Currently only support json and yaml"
+        end
+      end
     end
 
     class S3file
-      def initialize(bucket, file_name, config_file)
-        S3connect.new(config_file)
+      def initialize(bucket, file_name, config)
+        S3connect.new(config)
         s3 = AWS::S3.new()
         @obj = s3.buckets[bucket].objects[file_name]
       end
 
-      def upload(data)
+      def upload(data, make_public = false)
         @data = data
         @obj.write(@data)
+        if make_public
+          @obj.acl = :public_read
+        end
+      end
+
       end
 
       def download()
@@ -56,7 +77,7 @@ module Capriza
       end
 
       def <=> (local_file)
-        if ! File.exist?(local_file)
+        unless File.exist?(local_file)
           raise "File #{local_file} does not exist"
         end
         @obj.etag.gsub(/\"/,"") == Digest::MD5.hexdigest(File.read(local_file))
