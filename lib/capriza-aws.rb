3b87956660af7da8cd5f21d5211e2034afc841e1
Alon Becker <alon@alonbecker.com>
2013-04-23 21:08:59 +0300

    fix

--- lib/capriza-aws.rb
+++ lib/capriza-aws.rb
@@ -57,11 +57,7 @@ module Capriza
       def upload(data, make_public = false)
         @data = data
         @obj.write(@data)
-        if make_public
-          @obj.acl = :public_read
-        end
-      end
-
+        @obj.acl = :public_read if make_public
       end
 
       def download()
